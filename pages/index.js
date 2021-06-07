25d37d5d4618b2dede0f85517d5f24b9f5c52495
Aaditya Jawahar Shah <aadityajshah@gmail.com>
2021-06-07 13:15:23 -0400

    Update homepage

--- pages/index.js
+++ pages/index.js
@@ -5,52 +5,55 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>AADITYASHAHdotCOM</title>
+        <link rel="icon" href="/face.jpeg" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <br/ ><a href="https://www.aadityashah.com">AADITYASHAHdotCOM! </a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          {' '}
+          <code className={styles.code}>developer.foodie.gadget freak.techie.enthusiast.</code>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Résumé &rarr;</h3>
+            <p>Download a copy of my résumé.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Blog &rarr;</h3>
+            <p>Read my rants...</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Reviews &rarr;</h3>
+            <p>Read my reviews on everything...</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>How-to's &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              How-to's for any technology...
             </p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
+      <p>
+        Copyright 2021 Aaditya Shah.&nbsp;
+        </p>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -59,6 +62,7 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
+        
       </footer>
     </div>
   )
